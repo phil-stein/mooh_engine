abb15541968ff47711eba752716eb8fe0ac7cffb
Phil-Laptop <philipp033.25@gmail.com>
2023-06-14 12:18:46 +0200

    LAPTOP: testing PUSH.bat another time again

--- PUSH.bat
+++ PUSH.bat
@@ -8,15 +8,15 @@ IF "%~1"=="" (
 )
 
 
-git add .
-git commit -m %1
-git push origin main
-
-git submodule foreach git add .
-git submodule foreach git commit -m %1
-git submodule foreach git push origin main
-  
-goto jmp_exit
+:: git add .
+:: git commit -m %1
+:: git push origin main
+:: 
+:: git submodule foreach git add .
+:: git submodule foreach git commit -m %1
+:: git submodule foreach git push origin main
+::   
+:: goto jmp_exit
 
 
 :: ELSE (
@@ -25,34 +25,68 @@ goto jmp_exit
   :: git submodule foreach git push origin main
 ::   goto jmp_exit
 :: 
-::   :: -- global --
-::   cd src\global
-::   echo -- global -- 
-::   goto submodule_push
-:: 
-::   :: -- math --
-::   cd src\math
-::   echo -- math -- 
-::   goto submodule_push
-::   
-::   :: -- phys --
-::   cd src\phys
-::   echo -- phys -- 
-::   goto submodule_push
-::     
-::   :: -- serialization --
-::   cd src\serialization
-::   echo -- serialization -- 
-::   goto submodule_push
-::   
-::   :: -- text --
-::   cd src\text
-::   echo -- text -- 
-::   goto submodule_push
-:: 
-::   :: -- main repo --
-::   goto push
-:: )
+:: -- global --
+cd src\global
+echo -- global -- 
+:: goto submodule_push
+  git checkout main
+  git add .
+  git commit -m %1
+  git push origin main
+  cd ..\..
+  git add .
+
+:: -- math --
+cd src\math
+echo -- math -- 
+:: goto submodule_push
+  git checkout main
+  git add .
+  git commit -m %1
+  git push origin main
+  cd ..\..
+  git add .
+
+
+:: -- phys --
+cd src\phys
+echo -- phys -- 
+:: goto submodule_push
+  git checkout main
+  git add .
+  git commit -m %1
+  git push origin main
+  cd ..\..
+  git add .     
+
+  :: -- serialization --
+cd src\serialization
+echo -- serialization -- 
+:: goto submodule_push
+  git checkout main
+  git add .
+  git commit -m %1
+  git push origin main
+  cd ..\..
+  git add .     
+
+:: -- text --
+cd src\text
+echo -- text -- 
+:: goto submodule_push
+  git checkout main
+  git add .
+  git commit -m %1
+  git push origin main
+  cd ..\..
+  git add .
+
+:: -- main repo --
+echo -- text -- 
+:: goto push
+git add .
+git commit -m %1
+git push origin main
 
 :: :push
 :: git add .
@@ -60,11 +94,11 @@ goto jmp_exit
 :: git push origin main
 
 :: :submodule_push
-:: git checkout main
-:: git add .
-:: git commit -m %1
-:: git push origin main
-:: cd ..\..
-:: git add .
+::   git checkout main
+::   git add .
+::   git commit -m %1
+::   git push origin main
+::   cd ..\..
+::   git add .
 
 :jmp_exit
